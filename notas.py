fe65efd4cb3c3086b37557f638f9e8a089797634
Karen <karenvizcaino0316@gmail.com>
2025-09-17 17:55:34 -0500

    Subir cambios recientes

--- notas.py
+++ notas.py
@@ -1,7 +1,7 @@
 #NOMBRE:ALGORITMONOTA
 #ENTRADA: ingresar una nota
 #SALIDA:identificar si atraves de la nota del estudiante aprueba o no
-#PROCESO:este algoritmo hace que el usuario a la hora de digitar su nota sabra si aprobo o no y que no puede ser una nota mayor de 5 ya que no seria valida
+#PROCESO: Este algoritmo hace que el usuario a la hora de digitar su nota sabra si aprobo o no y que no puede ser una nota mayor de 5 ya que no seria valida
 numero1 = float(input("ingrese la nota del estudiante"))
 if numero1>= 0 and numero1 <= 5 or numero1 >=3:
     print("aprobaste")
